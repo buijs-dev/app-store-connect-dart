8f91d5b4593973a290882125d4c284a4f8a9f6d8
buijs <jjgbuijs@gmail.com>
2022-04-29 15:24:09 +0200

    Implement BaseService to execute GET and POST requests against a given AppleClient.

---
+++ lib/src/base/base_service.dart
@@ -0,0 +1,76 @@
+// Copyright (c) 2021 - 2022 Buijs Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import 'package:appstoreconnect/src/credentials.dart';
+import 'package:appstoreconnect/src/utils/client.dart';
+import 'package:http/http.dart';
+
+/// [Author] Gillian Buijs.
+class BaseService {
+  BaseService({
+    required this.credentials,
+    required this.path,
+    required this.client,
+  });
+
+  /// Credentials used to create a JSON web token for authenticating.
+  final AppleCredentials credentials;
+
+  /// Resource path which is appended to the base App Store Connect API endpoint.
+  final String path;
+
+  /// Client which communicates with the App Store Connect API.
+  ///
+  /// Defaults to [HttpAppleClient].
+  final AppleClient client;
+
+  /// Query parameters.
+  var query = <String, String>{};
+
+  /// Resource URI e.g. base endpoint + path.
+  String get _uri => "https://api.appstoreconnect.apple.com/v1/$path$_printQuery";
+
+  /// Print the query params or return an empty String if there are none.
+  String get _printQuery {
+    var printed = "?";
+
+    // Join the map to a single query String.
+    query.forEach((key, value) => printed += "$key=$value");
+
+    // Clear the query for the next requests.
+    query.clear();
+
+    // If there are no query params then return an empty String.
+    return printed == "?" ? "" : printed;
+  }
+
+  /// Execute a GET request to App Store Connect API.
+  Future<Response> get doGet async => client.get(
+    uri: _uri,
+    jwt: credentials.jsonWebToken,
+  );
+
+  /// Execute a POST request to App Store Connect API.
+  Future<Response> doPost(String body) async => client.post(
+    uri: _uri,
+    jwt: credentials.jsonWebToken,
+    body: body,
+  );
+
+}
