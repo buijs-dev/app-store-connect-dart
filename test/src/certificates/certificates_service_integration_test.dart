18a26e90e4de33cb10a85d7da7b42db745b6ff03
buijs <jjgbuijs@gmail.com>
2022-04-30 22:04:28 +0200

    Add integration test to create, find and revoke a signing certificate.

---
+++ test/src/certificates/certificates_service_integration_test.dart
@@ -0,0 +1,116 @@
+// Copyright (c) 2021 - 2022 Buijs Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import 'dart:io';
+
+import 'package:appstoreconnect/appstoreconnect.dart';
+import 'package:appstoreconnect/src/utils/nullsafe.dart';
+import 'package:test/test.dart';
+
+/// Integration Test which uses a real Apple Developer account.
+void main() async {
+
+  final service = CertificatesService(AppStoreCredentials.fromFile("apple_keys.json"));
+
+  final csrContent = NotNull(File("private.csr")).map((file) {
+    return (file as File).readAsStringSync();
+  });
+
+  /// Get the certificate count to check if the count after creation is increased by 1.
+  int certificateCount = await service.find().then((res) => res.data.length);
+
+  /// Shared between tests.
+  Certificate? certificate;
+
+  /// Create a new signing certificate.
+  test('Verify create', () async {
+
+    final response = await service.create(
+        certificateType: CertificateType.iosDistribution,
+        csrContent: csrContent,
+    );
+
+    expect(response.data.type, "certificates");
+
+    certificate = response.data;
+
+  });
+
+  /// Lookup al certificates and check there is 1 certificate more than before.
+  test('Verify find', () async {
+
+    final response = await service.find();
+
+    expect(response.data.length, certificateCount + 1);
+
+  });
+
+  test('Verify find with query', () async {
+
+    final response = await service.find((_) => _
+      ..filterId = [certificate!.id]
+      ..filterSerialNumber = [certificate!.attributes.serialNumber!]
+      ..filterDisplayName = [certificate!.attributes.displayName!]
+      ..limit = 1
+    );
+
+    final certificate2 = response.data[0];
+    expect(certificate!.toString(), certificate2.toString());
+
+  });
+
+  /// Search the new signing certificate by using the ID.
+  test('Verify findById', () async {
+
+    final response = await service.findById(certificate!.id);
+
+    final certificate2 = response.data;
+    expect(certificate!.toString(), certificate2.toString());
+
+  });
+
+  test('Verify findById with showQuery', () async {
+
+    final response = await service.findById(certificate!.id,
+        show: (_) => _..showName
+    );
+
+    final certificate2 = response.data;
+    expect(certificate!.attributes.name, certificate2.attributes.name);
+
+  });
+
+  test('Verify revoke', () async {
+
+    final isRevoked = await service.revokeById(certificate!.id);
+
+    expect(isRevoked, true);
+
+  });
+
+  /// Lookup al certificates and check the total count is the same as when the test started.
+  test('Verify find', () async {
+
+    final response = await service.find();
+
+    expect(response.data.length, certificateCount);
+
+  });
+
+}
