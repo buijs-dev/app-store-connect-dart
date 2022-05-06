8f2b77d8809609ed27a68ed14868c2d10088f6a0
buijs <jjgbuijs@gmail.com>
2022-05-06 15:26:14 +0200

    use CONST parent

--- lib/src/certificates/const.dart
+++ lib/src/certificates/const.dart
@@ -1,4 +1,23 @@
-import 'exception.dart';
+// Copyright (c) 2021 - 2022 Buijs Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import '../../connect.dart';
 
 typedef _C = CertificateType;
 
@@ -7,10 +26,8 @@ typedef _C = CertificateType;
 /// Source: https://developer.apple.com/documentation/appstoreconnectapi/certificatetype.
 ///
 /// [Author] Gillian Buijs.
-class CertificateType {
-  const CertificateType(this.value);
-
-  final String value;
+class CertificateType extends CONST {
+  const CertificateType(String value): super(value);
 
   static const developerIdApplication = _C("DEVELOPER_ID_APPLICATION");
   static const developerIdKext = _C("DEVELOPER_ID_KEXT");
